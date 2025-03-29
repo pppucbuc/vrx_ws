1efa34b3a48da2ca108156b2236d10274f6a41b0
neofelis <ddddyn@umich.edu>
2025-03-29 16:10:05 -0400

    Initial commit

---
+++ build/wamv_gazebo/colcon_command_prefix_build.sh
@@ -0,0 +1,3 @@
+# generated from colcon_core/shell/template/command_prefix.sh.em
+. "/home/neofelis/vrx_ws/install/share/vrx_gazebo/package.sh"
+. "/home/neofelis/vrx_ws/install/share/wamv_description/package.sh"
